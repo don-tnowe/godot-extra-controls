fd3ef0b93ed67c8184e610a08a7dc258218b10e4
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2024-07-23 18:18:14 +0300

    Allow proportion-based scaling to MaxSizeContainer

--- addons/tnowe_extra_controls/elements/max_size_container.gd
+++ addons/tnowe_extra_controls/elements/max_size_container.gd
@@ -4,11 +4,17 @@ extends Container
 
 ## Container that stretches children to fit, but only up to its set max size.
 
-## Maximum size of child controls. If -1 on either axis, does not limit size on that axis. [br]
+## Maximum size of child controls. If -1 on either axis, does not affect size on that axis. [br]
 ## [b]Note:[/b] size cannot go under the control's own minimum size.
 @export var max_size := -Vector2.ONE:
 	set(v):
 		max_size = v
+		queue_sort()
+## Use [member max_size] as an aspect ratio instead. One axis of [member max_size] must be -1. [br]
+## For example, [code](2.5, -1)[/code] will ensure the X axis is no more than 2.5x the width of the Y axis.
+@export var proportional_size := false:
+	set(v):
+		proportional_size = v
 		queue_sort()
 
 
@@ -20,11 +26,28 @@ func _notification(what : int):
 
 
 func _resize_child(child : Control):
+	if max_size.x < 0.0 && max_size.y < 0.0:
+		# Don't affect either side.
+		fit_child_in_rect(child, Rect2(Vector2.ZERO, size))
+		return
+
 	var minsize := child.get_combined_minimum_size()
-	var result_size := Vector2(
-		max(size.x if max_size.x < 0 else min(max_size.x, size.x), minsize.x),
-		max(size.y if max_size.y < 0 else min(max_size.y, size.y), minsize.y),
-	)
+	var result_size := Vector2()
+
+	if proportional_size:
+		result_size = size
+		if max_size.x < 0.0 && size.y / size.x > max_size.y:
+			result_size.y = size.x * max_size.y
+
+		if max_size.y < 0.0 && size.x / size.y > max_size.x:
+			result_size.x = size.y * max_size.x
+
+	else:
+		result_size = Vector2(
+			maxf(size.x if max_size.x < 0.0 else minf(max_size.x, size.x), minsize.x),
+			maxf(size.y if max_size.y < 0.0 else minf(max_size.y, size.y), minsize.y),
+		)
+
 	fit_child_in_rect(child, Rect2((size - result_size) * 0.5, result_size))
 
 
