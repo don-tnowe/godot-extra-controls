3eef9b0b831b80ae9358cc691307263086ee07de
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2024-07-23 23:44:02 +0300

    Finish up radial container

--- addons/tnowe_extra_controls/elements/radial_container.gd
+++ addons/tnowe_extra_controls/elements/radial_container.gd
@@ -2,9 +2,9 @@
 class_name RadialContainer
 extends Container
 
-## Radial menu container. Radius is based on own size. Supports texturing and [Control.size_flags_stretch_ratio].
+## Radial menu container. Radius is based on own size. Supports texturing, tweening and [Control.size_flags_stretch_ratio].
 
-## Behaviour when a node goes beyond the size of a specified array
+## Behaviour when an index goes beyond the size of an array.
 enum OutOfBoundsBehaviour {
 	CLAMP, ## Use the last array item.
 	REPEAT, ## Repeat from index 0 in the same order.
@@ -17,82 +17,48 @@ enum OutOfBoundsBehaviour {
 		progress_offset = v
 		queue_sort()
 		queue_redraw()
-
-@export_group("Dimensions")
-@export var radius_factor_outer := 1.0:
+## Width of the border in between sectors.
+@export var border_between_width := 4.0:
 	set(v):
 		if v < 0.0: v = 0.0
-		radius_factor_outer = v
-		queue_sort()
-@export var item_radius_inner := 0.0:
-	set(v):
-		if v < 0.0: v = 0.0
-		item_radius_inner = v
-		queue_sort()
-@export var item_radius_factor := 0.5:
-	set(v):
-		if v < 0.0: v = 0.0
-		item_radius_factor = v
-		queue_sort()
-@export var item_radius_factor_per : Array[float] = [1.0]:
-	set(v):
-		item_radius_factor_per = v
-		queue_sort()
-@export var item_radius_factor_per_behaviour := OutOfBoundsBehaviour.CLAMP:
-	set(v):
-		item_radius_factor_per_behaviour = v
-		queue_sort()
-
-@export var border_width := 4.0:
-	set(v):
-		if v < 0.0: v = 0.0
-		border_width = v
+		border_between_width = v
 		queue_redraw()
-@export var sector_points := 60:
+## Minimum total number of points on the circle.
+@export_range(2, 3600) var sector_points := 60:
 	set(v):
 		if v < 2: v = 2
 		sector_points = v
 		queue_redraw()
-@export var sector_radius_inner := 0.0:
-	set(v):
-		if v < 0.0: v = 0.0
-		sector_radius_inner = v
-		queue_redraw()
-@export_range(0.0, 1.0) var sector_radius_inner_factor := 0.0:
-	set(v):
-		sector_radius_inner_factor = v
-		queue_redraw()
-@export var outline_width := 0.0:
-	set(v):
-		if v < 0.0: v = 0.0
-		outline_width = v
-		queue_redraw()
 
-@export_group("Style")
-@export var colors : Array[Color] = [Color.WHITE]:
+## [RadialContainerTheme] applied to all objects.
+@export var radial_theme : RadialContainerTheme:
 	set(v):
-		colors = v
-		queue_redraw()
-@export var colors_behaviour := OutOfBoundsBehaviour.CLAMP:
+		if radial_theme != null:
+			radial_theme.changed.disconnect(_on_radial_theme_changed)
+
+		radial_theme = v
+		if v != null:
+			v.changed.connect(_on_radial_theme_changed)
+
+		_on_radial_theme_changed()
+## [RadialContainerTheme] applied to each individual child. Color, textures and item positions are overriden entirely, while others are combined with [member radial_theme].
+@export var radial_theme_per : Array[RadialContainerTheme]:
 	set(v):
-		colors_behaviour = v
-		queue_redraw()
-@export var outline_colors : Array[Color] = [Color.WHITE]:
+		for x in radial_theme_per:
+			if x != null && x.changed.is_connected(_on_radial_theme_changed):
+				x.changed.disconnect(_on_radial_theme_changed)
+
+		radial_theme_per = v
+		for x in v:
+			if x != null && !x.changed.is_connected(_on_radial_theme_changed):
+				x.changed.connect(_on_radial_theme_changed)
+
+		_on_radial_theme_changed()
+## Behaviour when new children get added, but [member radial_theme_per] is not expanded.
+@export var radial_theme_per_behaviour : OutOfBoundsBehaviour:
 	set(v):
-		outline_colors = v
-		queue_redraw()
-@export var outline_colors_behaviour := OutOfBoundsBehaviour.CLAMP:
-	set(v):
-		outline_colors_behaviour = v
-		queue_redraw()
-@export var textures : Array[Texture2D] = []:
-	set(v):
-		textures = v
-		queue_redraw()
-@export var textures_behaviour := OutOfBoundsBehaviour.CLAMP:
-	set(v):
-		textures_behaviour = v
-		queue_redraw()
+		radial_theme_per_behaviour = v
+		_on_radial_theme_changed()
 
 var _child_stretch_ratio_sum := 0.0
 var _child_stretch_sum_until : Array[float] = []
@@ -119,31 +85,68 @@ func get_index_from_direction(direction : Vector2):
 
 	return 0
 
-## Returns the radius of the empty space inside the wheel.
-func get_inner_radius():
-	var outer_radius := minf(size.x, size.y) * 0.5 * radius_factor_outer
-	return clampf(sector_radius_inner_factor * outer_radius + sector_radius_inner, 2.0 * border_width, outer_radius)
+## Set the [RadialContainerTheme] for a specific sector, updating all visuals.
+func set_theme_at(index : int, new_theme : RadialContainerTheme):
+	if index >= radial_theme_per.size():
+		var new_themes := radial_theme_per.duplicate()
+		var count := radial_theme_per.size()
+		while index >= count:
+			radial_theme_per.append(get_index_with_oob(count, new_themes, radial_theme_per_behaviour))
+			count += 1
 
+	radial_theme_per[index] = new_theme
+	radial_theme_per = radial_theme_per
 
-static func get_index_with_oob(index : int, max_index : int, oob : OutOfBoundsBehaviour):
-	if max_index <= 1:
-		return 0
+## Set the [RadialContainerTheme] for a specific sector, animated. A copied theme will be created, its properties will transition smoothly to the specified theme's properties.
+func set_theme_tweened(
+	index : int, new_theme : RadialContainerTheme, tween_time : float,
+	tween_trans := Tween.TRANS_LINEAR, tween_ease := Tween.EASE_OUT
+) -> Tween:
+	if index >= radial_theme_per.size():
+		var new_themes := radial_theme_per.duplicate()
+		var count := radial_theme_per.size()
+		while index >= count:
+			radial_theme_per.append(get_index_with_oob(count, new_themes, radial_theme_per_behaviour))
+			count += 1
+
+	var new_new_theme := RadialContainerTheme.new()
+	var tween := create_tween().set_ease(tween_ease).set_trans(tween_trans)
+	var old_theme := radial_theme_per[index] if radial_theme_per[index] != null else radial_theme
+	tween.tween_method(func(x : float): new_new_theme.lerp_theme(old_theme, new_theme, x), 0.0, 1.0, tween_time)
+
+	radial_theme_per[index] = new_new_theme
+	radial_theme_per = radial_theme_per
+	return tween
+
+## Utility function to apply [enum OutOfBoundsBehaviour] to an array index.
+static func get_index_with_oob(index : int, array : Array, oob : OutOfBoundsBehaviour, default_if_empty = null):
+	var max_index := array.size()
+	if max_index < 1:
+		return default_if_empty
 
 	match oob:
 		OutOfBoundsBehaviour.CLAMP:
-			return mini(index, max_index - 1)
+			return array[mini(index, max_index - 1)]
 		OutOfBoundsBehaviour.REPEAT:
-			return index % max_index
+			return array[index % max_index]
 		OutOfBoundsBehaviour.PINGPONG:
 			var iter := (index) / (max_index - 1)
 			if iter % 2 == 0:
-				return index % (max_index - 1)
+				return array[index % (max_index - 1)]
 			else:
-				return (max_index - 1) - index % (max_index - 1)
+				return array[(max_index - 1) - index % (max_index - 1)]
+
+## Utility function to fill the array with one value but a specific index with another value.
+static func set_all_except(all_value, except_value, all_count : int, except_index : int, array : Array):
+	if all_count > 1:
+		array.resize(all_count)
+
+	array.fill(all_value)
+	array[except_index] = except_value
 
 
 func _has_point(point : Vector2):
-	var radius := minf(size.x, size.y) * 0.5 * radius_factor_outer
+	var radius := minf(size.x, size.y) * 0.5
 	return (point - size * 0.5).length_squared() <= radius * radius
 
 
@@ -166,51 +169,69 @@ func _notification(what : int):
 			if x is Control && x.visible:
 				_child_stretch_sum_until[child_index] = child_stretch_ratio_sum_current
 				child_stretch_ratio_sum_next = child_stretch_ratio_sum_current + x.size_flags_stretch_ratio / _child_stretch_ratio_sum
-				sort_child(x, child_index, child_stretch_ratio_sum_current, child_stretch_ratio_sum_next)
+				_sort_child(x, child_index, child_stretch_ratio_sum_current, child_stretch_ratio_sum_next)
 				child_stretch_ratio_sum_current = child_stretch_ratio_sum_next
 				child_index += 1
 
 		queue_redraw()
 
 
-func sort_child(child : Control, child_index : int, progress_min : float, progress_max : float):
-	var radius := minf(size.x, size.y) * 0.5 * radius_factor_outer
-	radius = lerp(item_radius_inner, radius, item_radius_factor)
-	if item_radius_factor_per.size() != 0:
-		radius *= item_radius_factor_per[get_index_with_oob(child_index, item_radius_factor_per.size(), item_radius_factor_per_behaviour)]
-
+func _sort_child(child : Control, child_index : int, progress_min : float, progress_max : float):
+	var radius := minf(size.x, size.y) * 0.5
+	var sector_theme : RadialContainerTheme = RadialContainerTheme.new().merge(
+		get_index_with_oob(child_index, radial_theme_per, radial_theme_per_behaviour, null),
+		radial_theme,
+	)
 	child.size = child.get_combined_minimum_size()
-	child.position = size * 0.5 + Vector2(radius, 0).rotated(TAU * (progress_min + progress_max + progress_offset + progress_offset) * 0.5) - child.size * 0.5
+	child.scale = sector_theme.item_scale
+	child.position = (
+		size * 0.5
+		+ Vector2(lerp(
+			sector_theme.radius_factor_inner,
+			sector_theme.radius_factor_outer,
+			sector_theme.item_radius_factor,
+			) * radius, 0)
+			.rotated(TAU * (progress_min + progress_max + progress_offset + progress_offset) * 0.5)
+		- child.size * child.scale * 0.5
+	)
 
 
 func _draw():
-	var radius := minf(size.x, size.y) * 0.5 * radius_factor_outer
-	var inner_radius_f := clampf(sector_radius_inner_factor + sector_radius_inner / radius, 2.0 * border_width / radius, 0.999 * radius_factor_outer)
-	var border_rad := asin(0.5 * border_width / radius) * 0.5
-	var border_rad_inner := asin(0.5 * border_width / (radius * inner_radius_f)) * 0.5
-	if is_nan(border_rad_inner): border_rad_inner = 0.0
+	var global_radius := minf(size.x, size.y) * 0.5
+	var inner_radius_f := 0.0
+	if radial_theme != null:
+		global_radius *= radial_theme.radius_factor_outer
+		inner_radius_f = radial_theme.radius_factor_inner
+
 	for i in _child_stretch_sum_until.size() - 1:
-		var start_rad := (progress_offset + _child_stretch_sum_until[i]) * TAU + border_rad
-		var end_rad := (progress_offset + _child_stretch_sum_until[i + 1]) * TAU - border_rad
+		var sector_theme : RadialContainerTheme = get_index_with_oob(i, radial_theme_per, radial_theme_per_behaviour, null)
+		if radial_theme == null:
+			inner_radius_f = 1.0
+
+		var border_angle_rad := asin(
+			0.5 * border_between_width
+			/ (global_radius * (1.0 if sector_theme == null else sector_theme.radius_factor_outer))
+		) * 0.5
+		var border_angle_rad_inner := asin(
+			0.5 * border_between_width
+			/ (global_radius * (1.0 if sector_theme == null else sector_theme.radius_factor_inner) * inner_radius_f)
+		) * 0.5
+		if is_nan(border_angle_rad_inner):
+			border_angle_rad_inner = 0.0
+
+		var start_rad := (progress_offset + _child_stretch_sum_until[i]) * TAU + border_angle_rad
+		var end_rad := (progress_offset + _child_stretch_sum_until[i + 1]) * TAU - border_angle_rad
 		if end_rad <= start_rad: continue
 
-		var start_rad_inner := (progress_offset + _child_stretch_sum_until[i]) * TAU + border_rad_inner
-		var end_rad_inner := (progress_offset + _child_stretch_sum_until[i + 1]) * TAU - border_rad_inner
+		var start_rad_inner := (progress_offset + _child_stretch_sum_until[i]) * TAU + border_angle_rad_inner
+		var end_rad_inner := (progress_offset + _child_stretch_sum_until[i + 1]) * TAU - border_angle_rad_inner
 		if end_rad_inner <= start_rad_inner: continue
 
 		var sector_points_cur := (maxi((end_rad - start_rad) / TAU * sector_points, 2)) * 2
 		var sector_points_step := 2.0 / (sector_points_cur - 2)
 
-		var result_color := Color.WHITE
-		if colors.size() != 0:
-			result_color = colors[get_index_with_oob(i, colors.size(), colors_behaviour)]
-
-		var result_tex : Texture2D = null
-		if textures.size() != 0:
-			result_tex = textures[get_index_with_oob(i, textures.size(), textures_behaviour)]
-
-		var result_positions := PackedVector2Array()
-		var result_uv := PackedVector2Array()
+		var result_positions : Array[Vector2] = []
+		var result_uv : Array[Vector2] = []
 		result_positions.resize(sector_points_cur)
 		result_uv.resize(sector_points_cur)
 
@@ -221,44 +242,79 @@ func _draw():
 			var pt_uv_inner := Vector2(cos(pt_angle_inner), sin(pt_angle_inner)) * inner_radius_f
 			result_uv[j] = (pt_uv + Vector2.ONE) * 0.5
 			result_uv[sector_points_cur - 1 - j] = (pt_uv_inner + Vector2.ONE) * 0.5
-			result_positions[j] = (pt_uv * radius + size * 0.5)
-			result_positions[sector_points_cur - 1 - j] = pt_uv_inner * radius + size * 0.5
 
-		draw_colored_polygon(
-			result_positions,
-			result_color,
-			result_uv,
-			result_tex,
-		)
-		if outline_width > 0.0:
-			var result_outline_color := Color.WHITE
-			if outline_colors.size() != 0:
-				result_outline_color = outline_colors[get_index_with_oob(i, outline_colors.size(), outline_colors_behaviour)]
-	
-			var result_positions_outline1 := PackedVector2Array()
-			var result_positions_outline2 := PackedVector2Array()
-			result_positions_outline1.resize(sector_points_cur)
-			result_positions_outline2.resize(sector_points_cur)
-			var outline_width_factor := outline_width / (radius * (1.0 - inner_radius_f))
-			for j in sector_points_cur / 2:
-				var alt_j := sector_points_cur - 1 - j
-				result_positions_outline1[j] = lerp(result_positions[alt_j], result_positions[j], outline_width_factor)
-				result_positions_outline1[alt_j] = result_positions[alt_j]
+			if sector_theme == null:
+				result_positions[j] = pt_uv * global_radius + size * 0.5
+				result_positions[sector_points_cur - 1 - j] = pt_uv_inner * global_radius + size * 0.5
 
-			for j in sector_points_cur / 2:
-				var alt_j := sector_points_cur - 1 - j
-				result_positions_outline2[j] = result_positions[j]
-				result_positions_outline2[alt_j] = lerp(result_positions[alt_j], result_positions[j], 1.0 - outline_width_factor)
+			else:
+				result_positions[j] = pt_uv * global_radius * sector_theme.radius_factor_outer + size * 0.5
+				result_positions[sector_points_cur - 1 - j] = pt_uv_inner * global_radius * sector_theme.radius_factor_inner + size * 0.5
 
+		if sector_theme != null:
 			draw_colored_polygon(
-				result_positions_outline1,
-				result_outline_color,
+				result_positions,
+				sector_theme.color,
 				result_uv,
-				null,
+				sector_theme.texture,
 			)
+
+		elif radial_theme != null:
 			draw_colored_polygon(
-				result_positions_outline2,
-				result_outline_color,
+				result_positions,
+				radial_theme.color,
 				result_uv,
-				null,
+				radial_theme.texture,
 			)
+
+		else:
+			draw_colored_polygon(
+				result_positions,
+				Color.WHITE,
+			)
+
+		# if outline_width > 0.0:
+		# 	_draw_outline()
+
+
+func _draw_outline(
+	i : int, outline_width : float, radius : float, inner_radius_f : float, result_outline_color : Color,
+	result_uv : Array[Vector2], result_positions : Array[Vector2], sector_points_cur : int
+):
+	var outline_width_factor := outline_width / (radius * (1.0 - inner_radius_f))
+	var result_positions_outline_o := PackedVector2Array()
+	result_positions_outline_o.resize(sector_points_cur)
+	for j in sector_points_cur / 2:
+		var alt_j := sector_points_cur - 1 - j
+		result_positions_outline_o[j] = result_positions[j]
+		result_positions_outline_o[alt_j] = lerp(result_positions[alt_j], result_positions[j], 1.0 - outline_width_factor)
+
+	draw_colored_polygon(
+		result_positions_outline_o,
+		result_outline_color,
+		result_uv,
+		null,
+	)
+
+	# if !outline_inner:
+	# 	return
+
+	var result_positions_outline_i := PackedVector2Array()
+	result_positions_outline_i.resize(sector_points_cur)
+
+	for j in sector_points_cur / 2:
+		var alt_j := sector_points_cur - 1 - j
+		result_positions_outline_i[j] = lerp(result_positions[alt_j], result_positions[j], outline_width_factor)
+		result_positions_outline_i[alt_j] = result_positions[alt_j]
+
+	draw_colored_polygon(
+		result_positions_outline_i,
+		result_outline_color,
+		result_uv,
+		null,
+	)
+
+
+func _on_radial_theme_changed():
+	queue_redraw()
+	queue_sort()
